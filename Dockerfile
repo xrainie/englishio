65865ecab43ec2be473ba7cf666663dc41482e67
xoxrainie <xoxrainie@gmail.com>
2024-07-23 19:33:57 +0300

    add local/prod settings & configure Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /code
+
+RUN pip install --upgrade pip
+
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+
+COPY . /code/
---
+++ englishio/settings/prod_settings.py
@@ -0,0 +1,15 @@
+from .base import *
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+ALLOWED_HOSTS = ['*']
