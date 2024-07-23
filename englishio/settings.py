cf805059e8e71de924e5eb9740b951bdcb58d36d
xoxrainie <xoxrainie@gmail.com>
2024-07-23 21:31:43 +0300

    migrate to postgresql database

--- englishio/settings.py
+++ englishio/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from django.conf.global_settings import CACHES, LOGIN_REDIRECT_URL, LOGIN_URL, LOGOUT_REDIRECT_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
--- englishio/settings/local_settings.py
+++ englishio/settings/local_settings.py
@@ -1,1 +1,13 @@
 from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'englishio1',
+        'USER': 'englishio',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
